b04b0d153f8b043a491ac89b82fb13370cacf795
dongkam <84716824+dongkam5@users.noreply.github.com>
2022-02-12 01:03:49 +0900

    커밋

--- prac373.py
+++ prac373.py
@@ -1,20 +1,20 @@
-#프로그래머스 섬 연결하기
-def solution(n, costs):
-    answer = 0
-    costs.sort(key=lambda x:(x[2]))
-    island={}
-    link=0
-    for cost in costs:
-        a,b,c=cost[0],cost[1],cost[2]
-        if a not in island or b not in island:
-            island[a]=b
-            island[b]=a
-            link+=1
-            answer+=c
-        else:
-            if link<n-1:
-                link+=1
-                answer+=c
-            else:
-                break
+# 프로그래머스 신고결과받기
+def solution(id_list, report, k):
+    answer = []
+    ban = {}
+    mail = {}
+    for id in id_list:
+        ban[id] = ''
+        mail[id] = 0
+    report = list(set(report))
+    for re in report:
+        a, b = re.split()
+        ban[b] += a+' '
+    for id in id_list:
+        l = list(map(str, ban[id].split()))
+        if(len(l) >= k):
+            for name in l:
+                mail[name] += 1
+    for val in mail:
+        answer.append(mail[val])
     return answer
