d9e7f8089364e8781ce3335f4c56828dd07fab4e
dongkam <84716824+dongkam5@users.noreply.github.com>
2022-05-24 11:49:03 +0900

    20220524

--- prac456.py
+++ prac456.py
@@ -1,4 +1,5 @@
 # 백준 2636 치즈
+from collections import deque
 import sys
 input = sys.stdin.readline
 dr = [0, 0, -1, 1]
@@ -6,41 +7,57 @@ dc = [1, -1, 0, 0]
 N, M = map(int, input().split())
 field = []
 cheese_cnt = 0
-answer = 0
 for _ in range(N):
     s = list(map(int, input().split()))
     for val in s:
         if val == 1:
             cheese_cnt += 1
     field.append(s)
+
+
+def bfs(start):
+    start_r, start_c = start
+    visited[start_r][start_c] = 1
+    q = deque([])
+    q.append((start_r, start_c))
+    while q:
+        curr_r, curr_c = q.popleft()
+        for k in range(4):
+            R = curr_r+dr[k]
+            C = curr_c+dc[k]
+            if 0 <= R < N and 0 <= C < M and visited[R][C] == 0:
+                if field[R][C] == 1:
+                    visited[R][C] = 1
+                    del_lst.append((R, C))
+                elif field[R][C] == 0:
+                    visited[R][C] = 1
+                    q.append((R, C))
+
+
 cnt = 0
 while True:
-    del_pos = []
-    for i in range(N):
-        for j in range(M):
-            if field[i][j] == 1:
-                for k in range(4):
-                    R = i+dr[k]
-                    C = j+dc[k]
-                    if 0 <= R < N and 0 <= C < M:
-                        if field[R][C] == 1:
-                            continue
-                        else:
-                            del_pos.append((i, j))
-                            cheese_cnt -= 1
-                            break
-                    else:
-                        continue
-    if del_pos:
-        cnt += 1
-        if cheese_cnt == 0:
-            print(del_pos)
-            answer = len(del_pos)
-            break
-        for pos in del_pos:
-            r, c = pos
-            field[r][c] = 0
-    else:
+    if cheese_cnt == 0:
         break
+    visited = [[0]*(M) for _ in range(N)]
+    del_lst = []
+    for i in range(N):
+        if field[i][0] == 0 and visited[i][0] == 0:
+            bfs((i, 0))
+    for i in range(N):
+        if field[i][M-1] == 0 and visited[i][M-1] == 0:
+            bfs((i, M-1))
+    for j in range(M):
+        if field[0][j] == 0 and visited[0][j] == 0:
+            bfs((0, j))
+    for j in range(M):
+        if field[N-1][j] == 0 and visited[N-1][j] == 0:
+            bfs((N-1, j))
+
+    del_cnt = len(del_lst)
+    for del_pos in del_lst:
+        r, c = del_pos
+        field[r][c] = 0
+    cheese_cnt -= del_cnt
+    cnt += 1
 print(cnt)
-print(answer)
+print(del_cnt)
