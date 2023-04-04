e34af9387a7b28cc650a25aba430f554122c7335
dongkam <84716824+dongkam5@users.noreply.github.com>
2023-04-04 10:40:43 +0900

    Create 3.py

---
+++ 0404/3.py
@@ -0,0 +1,13 @@
+#프로그래머스 level1 크기가 작은 부분 문자열
+def solution(t, p):
+    answer = 0
+    len_p=len(p)
+    len_t=len(t)
+    p=int(p)
+    i=0
+    while i<len_t-len_p+1:
+        temp=t[i:i+len_p]
+        if int(temp)<=p:
+            answer+=1
+        i+=1
+    return answer
